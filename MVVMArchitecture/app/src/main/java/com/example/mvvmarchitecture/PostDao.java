f6a3923864ec7ee9b8ff3f49f460ce8d2f54ec56
hojoungjang <jang88@purdue.edu>
2020-02-28 09:12:55 -0500

    create DAO interface

---
+++ MVVMArchitecture/app/src/main/java/com/example/mvvmarchitecture/PostDao.java
@@ -0,0 +1,21 @@
+package com.example.mvvmarchitecture;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface PostDao {
+    // conflict resolution strategy
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(PostData pData);
+
+    @Query("DELETE FROM post_data_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM post_data_table")
+    List<PostData> selectAll();
+}
--- MVVMArchitecture/app/src/main/java/com/example/mvvmarchitecture/PostData.java
+++ MVVMArchitecture/app/src/main/java/com/example/mvvmarchitecture/PostData.java
@@ -5,16 +5,19 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "user_data_table")
-public class UserData {
+@Entity(tableName = "post_data_table")
+public class PostData {
     @PrimaryKey
     @ColumnInfo(name = "user_id")
     private int userId;
+    @ColumnInfo(name = "id")
     private int id;
+    @ColumnInfo(name = "title")
     private String title;
+    @ColumnInfo(name = "body")
     private String body;
 
-    public UserData(int uid, int id, String title, String body) {
+    public PostData(int uid, int id, String title, String body) {
         this.userId = uid;
         this.id = id;
         this.title = title;
