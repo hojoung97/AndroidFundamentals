f9f0ec442add264f3d49d7a90dd10379aee834a4
hojoungjang <jang88@purdue.edu>
2020-02-28 23:23:49 -0500

    add RecyclerView

--- MVVMArchitecture/app/src/main/java/com/example/mvvmarchitecture/MainActivity.java
+++ MVVMArchitecture/app/src/main/java/com/example/mvvmarchitecture/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mvvmarchitecture;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // RecyclerView
+        RecyclerView postList = findViewById(R.id.post_list);
+        final PostListAdapter listAdapter = new PostListAdapter(this);
+        postList.setAdapter(listAdapter);
+        postList.setLayoutManager(new LinearLayoutManager(this));
     }
 }
